cf5d407f62b1838f01d72493acf21fc570199cb4
Aman <Palaman2626@gmail.com>
2024-01-24 14:48:44 +0530

    fixed the error

--- src/components/index.js
+++ src/components/index.js
@@ -6,6 +6,6 @@ import Projects from "./Projects/Projects";
 import Cursor from "./Cursor/Cursor";
 import Skills from "./Skills/Skills";
 import Navbar from "./Overlay/Navbar";
-import TechStack from "./techStack/TechStack";
+import TechStack from "./TechStack/TechStack";
 
 export {Hero,About,Parallax,Contact,Projects,Cursor,Skills,Navbar,TechStack};
