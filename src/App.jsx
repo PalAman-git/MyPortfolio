47b4c90f0319fc37f4936db88f4d6df9c121d080
Aman <Palaman2626@gmail.com>
2025-07-02 15:45:26 +0530

    removed the border

--- src/App.jsx
+++ src/App.jsx
@@ -46,7 +46,7 @@ function App() {
   return loading ? (
     <Loader />
   ) : (
-    <div className="app-container border-2 border-red-500">
+    <div className="app-container">
       <div>
         <Toaster
           position="top-center"
