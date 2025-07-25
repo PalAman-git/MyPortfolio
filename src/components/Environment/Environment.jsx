dff9d379db242c269b04b418f89f2b6eb3269fac
Aman <Palaman2626@gmail.com>
2025-07-25 07:51:45 +0530

    canvas color changed

--- src/components/Environment/Environment.jsx
+++ src/components/Environment/Environment.jsx
@@ -1,20 +1,21 @@
-import './style.css'
-import { Canvas } from '@react-three/fiber'
-import Portal from './Portal/Portal'
+import "./style.css";
+import { Canvas } from "@react-three/fiber";
+import Portal from "./Portal/Portal";
+import { Html } from "@react-three/drei";
 
 const Environment = () => {
   return (
     <>
-      <Canvas className='canvas' camera={{ position: [0, 0.4, 0.25], fov: 50}}>
+      <Canvas
+        className="canvas relative"
+        camera={{ position: [0, 0.4, 0.25], fov: 50 }}
+      >
         {/* <ambientLight intensity={0.5} /> */}
-        <color attach="background" args={['#000']} />
+        <color attach="background" args={["#eab539"]} />
         <Portal />
       </Canvas>
     </>
-  )
-}
+  );
+};
 
-
-
-
-export default Environment
+export default Environment;
